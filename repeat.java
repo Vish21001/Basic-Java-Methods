670ab477867053709f20464f9ac2e1c1c4cdfc78
Vish21001 <agarwalvishisht@gmail.com>
2025-12-06 11:09:15 -0500

    Repeats a string n number of times

---
+++ repeat.java
@@ -0,0 +1,9 @@
+public String repeatString(String str, int n)
+{
+    String repeatString = "";
+    for(int i = 0; i < n; i++)
+    {
+        repeatString += str;
+    }
+    return repeatString;
+}
